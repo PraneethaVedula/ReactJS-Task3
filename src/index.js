ac5b9451f64279197e031390186100642fb067cb
Praneetha <110773908+PraneethaVedula@users.noreply.github.com>
2023-10-14 21:20:51 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,21 +7,3 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App />   
 );
-
-
-
-
-
-
-
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
