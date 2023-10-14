e8891ecd6b1f85418e4c8bfd213598937c64bf51
PraneethaVedula <praneethavedula@gmail.com>
2023-10-14 21:18:33 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import './App.css';
+import colorArray from './Colors.js' ;
+import List from './List';
+
+function App() {
+  const [color, setColor] = useState(colorArray);
+  const [showList, setShowList] = useState(false);
+
+  const handleButtonClick = () => {
+    setShowList(!showList);
+  };
+
+  return (
+    <center>
+      <div id='container'>
+        {showList && <List colors={color}/>}
+      </div>
+        <p>Praneetha Vedula<br/>21BCE9304</p>
+      <h3 id="display"></h3>
+      <button onClick={handleButtonClick}>Pick a color</button>
+    </center>
+  );
+}
+
+export default App;
+
